bb930d1046d1b3d76f112fcd2e0da392a5a022fa
Aaron Beitch <aaronb@arista.com>
2020-12-15 15:29:16 -0800

    implement day13 part2

--- day13/main.go
+++ day13/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -14,12 +15,57 @@ func main() {
 		panic(err)
 	}
 
-	s := bytes.Split(b, []byte{'\n'})
+	part2(bytes.Split(b, []byte{'\n'}))
+}
+
+func part2(s [][]byte) {
+	type pair struct {
+		id     int
+		offset int
+	}
+	var ids []pair
+	for i, idB := range bytes.Split(s[1], []byte{','}) {
+		if idB[0] == 'x' {
+			continue
+		}
+		id, err := strconv.Atoi(string(idB))
+		if err != nil {
+			panic(err)
+		}
+		offset := (id - i) % id
+		if offset < 0 {
+			offset += id
+			if offset < 0 {
+				panic("huh")
+			}
+		}
+		ids = append(ids, pair{id, offset})
+	}
+
+	sort.Slice(ids, func(i, j int) bool {
+		return ids[j].id < ids[i].id
+	})
+	candidate := ids[0].offset
+	step := ids[0].id
+	for _, id := range ids[1:] {
+		fmt.Println("candidate:", candidate, "step:", step)
+		fmt.Printf("Looking for %d mod %d\n", id.offset, id.id)
+		for {
+			if candidate%id.id == id.offset {
+				break
+			}
+			candidate += step
+		}
+		step *= id.id
+	}
+	fmt.Println(candidate)
+}
+
+func part1(s [][]byte) {
 	t, err := strconv.Atoi(string(s[0]))
 	if err != nil {
 		panic(err)
 	}
-
 	minWait := 0xFFFFFFFF
 	minID := 0
 	for _, idB := range bytes.Split(s[1], []byte{','}) {
