5b9a028457e4f8c76fcf3a19344dfe99c9d3caab
Aaron Beitch <aaronb@arista.com>
2020-12-16 13:24:43 -0800

    complete day14 part 2

--- day14/main.go
+++ day14/main.go
@@ -21,13 +21,13 @@ func main() {
 		line := s.Text()
 		switch line[:4] {
 		case "mask":
-			c.applyMask(strings.Split(line, " = ")[1])
+			c.applyMask2(strings.Split(line, " = ")[1])
 		case "mem[":
 			var addr, value uint64
 			if _, err := fmt.Sscanf(line, "mem[%d] = %d", &addr, &value); err != nil {
 				panic(err)
 			}
-			c.set(addr, value)
+			c.set2(addr, value)
 		}
 	}
 
@@ -37,7 +37,24 @@ func main() {
 type computer struct {
 	zeroMask uint64
 	oneMask  uint64
+	floats   []uint8
 	memory   map[uint64]uint64
+}
+
+func (c *computer) applyMask2(s string) {
+	if len(s) != 36 {
+		panic(fmt.Errorf("wrong mask length: %d %q", len(s), s))
+	}
+	c.oneMask = 0
+	c.floats = nil
+	for i, r := range s {
+		switch r {
+		case 'X':
+			c.floats = append(c.floats, uint8(35-i))
+		case '1':
+			c.oneMask ^= 1 << (35 - i)
+		}
+	}
 }
 
 func (c *computer) applyMask(s string) {
@@ -53,6 +70,33 @@ func (c *computer) applyMask(s string) {
 		case '1':
 			c.oneMask ^= 1 << (35 - i)
 		}
+	}
+}
+
+func flipBits(in <-chan uint64, out chan<- uint64, index uint8) {
+	for addr := range in {
+		out <- addr
+		out <- addr ^ (1 << index)
+	}
+	close(out)
+}
+
+func (c *computer) genAddrs(addr uint64) <-chan uint64 {
+	ch := make(chan uint64, 1)
+	ch <- addr
+	close(ch)
+	for _, index := range c.floats {
+		out := make(chan uint64)
+		go flipBits(ch, out, index)
+		ch = out
+	}
+	return ch
+}
+
+func (c *computer) set2(addr, value uint64) {
+	addr |= c.oneMask
+	for addr := range c.genAddrs(addr) {
+		c.memory[addr] = value
 	}
 }
 
